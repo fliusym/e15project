25051733846d765f0747ef3d7b661f576f023f1d
Feng Liu <liufsym@gmail.com>
2014-09-14 12:39:59 -0700

    Project files

---
+++ p3/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>Project 3</title>
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+		<style>
+			h1{
+				text-align: center;
+			}
+
+			img{
+				display: block;
+				margin: auto;
+				width: 50%;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h1>E15 Project 3</h1>	
+
+		</div>
+
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
