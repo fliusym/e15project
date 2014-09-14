be2c1740f20fff6729c1c45ea2cf5ebc72953d17
Feng Liu <liufsym@gmail.com>
2014-09-14 09:31:24 -0700

    project files

--- p1/index.php
+++ p1/index.php
@@ -4,18 +4,35 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<title>My first</title>
+		<title>E15 Project</title>
 		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="../css/projectstyle.css">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+		<style>
+			h1{
+				text-align: center;
+			}
+
+			img{
+				display: block;
+				margin: auto;
+				width: 50%;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="container">
 			<h1>Frank Liu <br>Dynamic Web Apps Portfolio</h1>	
-			<img src="../pj.jpg" alt='Frank Liu'>
+			<img src="pj.jpg" alt='Frank Liu'>
 		</div>
 
 
-		<script src="../js/jquery.js"></script>
-		<script type="text/javascript" src="../js/bootstrap.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
