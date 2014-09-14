b36212516692687f3755b356355e9122c80dbd33
Feng Liu <liufsym@gmail.com>
2014-09-14 08:47:32 -0700

    index page

---
+++ p1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>My first</title>
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../css/projectstyle.css">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Frank Liu <br>Dynamic Web Apps Portfolio</h1>	
+			<img src="../pj.jpg" alt='Frank Liu'>
+		</div>
+
+
+		<script src="../js/jquery.js"></script>
+		<script type="text/javascript" src="../js/bootstrap.js"></script>
+	</body>
+</html>
