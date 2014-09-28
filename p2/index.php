1d4796ca604aed0074cfc06dceac725818b25327
Feng Liu <liufsym@gmail.com>
2014-09-28 11:00:57 -0400

    project 2

--- p2/index.php
+++ p2/index.php
@@ -1,11 +1,18 @@
+<?php
+    error_reporting(-1);
+    ini_set('display_errors',1);
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<title>Project 2</title>
+		<title>xkcd Password Generator</title>
 		
+		<?php 
+			require('logic.php');
+		?>
 		
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -18,12 +25,58 @@
 	</head>
 	<body>
 		<div class="container">
-			<h1>E15 Project 2</h1>	
+			<h1>xkcd Password Generator</h1>
+			<div class='row'>
+				<form class='form-horizontal' action='index.php' method='post'>
+					<div class='form-group'>
+						<label class="col-xs-2" for="numberOfWords">Number of Words:</label>
+						<div class="col-md-4">
+						    <input type="number" id="numberOfWords"  name="numberOfWords" class="form-control" required max="9" min="1" placeholder="Max number is 9">
+						</div>
+
+					</div>
+
+					<div class="col-xs-12" >
+					    <input type="checkbox" id="specialCharacter" name="addSpecialCharacter" >Special Character
+					    
+					</div>
+					<div class="col-xs-12" >
+					    
+					    <input type="checkbox" name="addNumber">Add Number
+					</div>  
+
+					<br>
+					<br>
+
+					<div class="col-xs-12 col-md-6 col-lg-4" >
+					    
+					    <button type="submit" class="btn btn-primary">Generate Password</button>
+					</div> 
+				</form>
+			</div>
+			<br>
+			<br>
+			<div class='row'>
+				<label class="col-xs-2" for="GeneratedPassword">Generated Password:</label>
+				<div class="col-md-6 col-xs-12">
+				   <input type="text" id='GeneratedPassword' readonly="true" class="form-control"  placeholder="Generated Password" value="<?php echo $password?>">
+				</div>			
+			</div>
+
+			<br>
+			<br>
+
+			<div class='row'>
+				<div class="col-xs-12 col-md-8">
+				    <img class="comic" src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>  
+				</div>
+			</div>
 
 		</div>
 
 
 		<!-- Latest compiled and minified JavaScript -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
---
+++ p2/logic.php
@@ -0,0 +1,61 @@
+<?php
+
+$wordsDic = array(
+'this',
+'is',
+'a',
+'just',
+'joke',
+'good',
+'man',
+'what',
+'when',
+'where',
+'dock',
+'damn'
+);
+
+$specialCharacters = array('@','$','%','&','*');
+$password = "";
+
+
+if(array_key_exists('numberOfWords',$_POST))
+{
+    //get the number of words
+    $number_words = $_POST['numberOfWords'];
+    //get random from words dictionary
+    $r = array_rand($wordsDic,$number_words);
+
+    //if it is array
+    if(is_array($r))
+    {
+        $password = $wordsDic[$r[0]];
+        //use - as default seperator
+        for($i = 1; $i < $number_words; $i++)
+        {
+            $password = $password . "-" . $wordsDic[$r[$i]];
+        }
+    }
+    else //it is a single character
+    {
+        $password = $wordsDic[$r];
+    }
+
+    //if add special charaacter is turned on add special char at the end
+    if(array_key_exists('addSpecialCharacter',$_POST))
+    {
+        $specialChar = $specialCharacters[array_rand($specialCharacters)];
+        $password = $password . $specialChar;
+    }
+    
+    //if add number is turned on add the number at the end
+    if(array_key_exists('addNumber',$_POST))
+    {
+        $digital = rand(1,9);
+        $password = $password . $digital;
+    }
+    
+}
+
+
+
