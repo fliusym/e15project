fa6be5960fd380ecee421e0ea1b1c601a52d2a60
Feng Liu <liufsym@gmail.com>
2014-12-15 10:51:05 -0500

    p4

---
+++ p4/app/config/production/database.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'mysql' => array('driver' => 'mysql' , 
+		'host' => 'localhost',
+		'database' => 'hiddengems',
+		'username' => 'root',
+		'password' => 'bl0ZIrDSrX'
+		),
+	);
